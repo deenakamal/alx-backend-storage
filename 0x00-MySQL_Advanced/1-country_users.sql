b937a0d6d937e2db18d40eff11bb933fb9c4023e
deenakamal <deenah.kamal@gmail.com>
2024-07-11 21:38:41 +0000

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates table user.
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM(US, CO, TN) DEAFULT 'US' NOT NULL,
+	PRIMARY KEY(id)
+);
