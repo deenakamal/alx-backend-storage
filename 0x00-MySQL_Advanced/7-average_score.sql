9c22eaa5f631f4eb995d91b82c1cdd832813b5d1
deenakamal <deenah.kamal@gmail.com>
2024-07-11 23:50:48 +0000

    7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- Creates a stored procedure ComputeAverageScoreForUser.
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE avgerage FLOAT;
+	SELECT AVG(score)
+	FROM corrections
+	WHERE correction.user_id = user_id;
+
+	UPDATE users
+	SET avarage_score = average
+	WHERE id = user_id
+END $$
+
+DELIMITER ;
