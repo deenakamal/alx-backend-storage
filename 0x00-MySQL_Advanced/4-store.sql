fb4579526a7f458299874b3285bb8501c1b2fbb4
deenakamal <deenah.kamal@gmail.com>
2024-07-11 22:26:10 +0000

    edit

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,9 +4,9 @@ DELIMITER //
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders FOR EACH ROW
 BEGIN
-	UPDATE items
-	SET quantity = quantity - 1 * NEW.number
-	WHERE name = NEW.name;
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.name;
 END //
 
-DELIMITER;
+DELIMITER ;
