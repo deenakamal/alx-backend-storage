67b502215a2840c57b0dad73827d2cb8f5218403
deenakamal <deenah.kamal@gmail.com>
2024-07-11 21:59:16 +0000

    task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- Lists all bands with GLam rock.
+SELECT band_name,
+     CASE
+         WHEN split IS NULL THEN 2022 - formed
+	 ELSE split - formed
+     END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%';
