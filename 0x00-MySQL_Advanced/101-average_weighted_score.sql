7724ed64af666574a116325d6abd32817e1f4bfb
deenakamal <deenah.kamal@gmail.com>
2024-07-12 01:04:01 +0000

    13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -4,29 +4,25 @@ DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE total_weighted_score FLOAT;
-    DECLARE total_weight FLOAT;
-    DECLARE average_weighted_score FLOAT;
+    DECLARE total_weighted_score INT;
+    DECLARE total_weight INT;
+    DECLARE average_weighted_score INT;
 
-    -- Calculate total weighted score for all users
     SELECT SUM(corrections.score * projects.weight)
     INTO total_weighted_score
     FROM corrections
     JOIN projects ON corrections.project_id = projects.id;
 
-    -- Calculate total weight across all projects
     SELECT SUM(weight)
     INTO total_weight
     FROM projects;
 
-    -- Handle division by zero scenario
     IF total_weight > 0 THEN
         SET average_weighted_score = total_weighted_score / total_weight;
     ELSE
         SET average_weighted_score = 0;
     END IF;
 
-    -- Update all users' average scores with calculated average
     UPDATE users
     SET average_score = average_weighted_score;
 END $$
