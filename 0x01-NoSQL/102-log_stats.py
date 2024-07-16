51a9fa3600671916941742df994b171567f3f6b2
deenakamal <deenah.kamal@gmail.com>
2024-07-16 23:41:43 +0000

    adv 3

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Module
+"""
+
+from pymongo import MongoClient
+
+
+HTTP_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+def display_nginx_log_stats(mongo_collection):
+    """ display log"""
+    total_log_count = mongo_collection.count_documents({})
+    print(f"{total_log_count} logs")
+
+    print("Methods:")
+    for method in HTTP_METHODS:
+        method_count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    status_check_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+
+    print("IPs:")
+    top_ips = mongo_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+def main():
+    """main"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+
+    display_nginx_log_stats(nginx_collection)
+
+
+if __name__ == "__main__":
+    main()
