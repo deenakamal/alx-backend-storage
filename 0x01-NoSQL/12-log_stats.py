eeaee34164c44a07af1155bf468fc9f360263642
deenakamal <deenah.kamal@gmail.com>
2024-07-16 23:17:37 +0000

    12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Module """
+from pymongo import MongoClient
+
+HTTP_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+def log_stats(mongo_collection):
+    """
+    Provide statistics about Nginx logs stored in MongoDB.
+    """
+    try:
+        # Total number of logs
+        total_logs = mongo_collection.count_documents({})
+        print(f"{total_logs} logs")
+
+        # Methods statistics
+        print("Methods:")
+        for method in HTTP_METHODS:
+            method_count = mongo_collection.count_documents({"method": method})
+            print(f"\tmethod {method}: {method_count}")
+
+        # Count of logs with method=GET and path=/status
+        status_check_count = mongo_collection.count_documents(
+                {"method": "GET", "path": "/status"}
+                )
+        print(f"method=GET, path=/status: {status_check_count} status check")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    try:
+        client = MongoClient('mongodb://localhost:27017')
+        db = client.logs
+        nginx_collection = db.nginx
+
+        log_stats(nginx_collection)
+
+    except Exception as e:
+        print(f"Error connecting to MongoDB: {e}")
