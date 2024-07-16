fb9f03af27f982cead0b693105217a2ec5c0b866
deenakamal <deenah.kamal@gmail.com>
2024-07-16 23:37:30 +0000

    edit 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,39 +1,35 @@
 #!/usr/bin/env python3
-""" Module """
+"""
+Moduel
+"""
 from pymongo import MongoClient
+
 
 HTTP_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
-def log_stats(mongo_collection):
-    """
-    Provide statistics about Nginx logs stored in MongoDB.
-    """
-    try:
-        total_logs = mongo_collection.count_documents({})
-        print(f"{total_logs} logs")
+def display_nginx_log_stats(mongo_collection):
+    """ display"""
+    total_log_count = mongo_collection.count_documents({})
+    print(f"{total_log_count} logs")
 
-        print("Methods:")
-        for method in HTTP_METHODS:
-            method_count = mongo_collection.count_documents({"method": method})
-            print(f"\tmethod {method}: {method_count}")
+    print("Methods:")
+    for method in HTTP_METHODS:
+        method_count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
 
-        status_check_count = mongo_collection.count_documents(
-                {"method": "GET", "path": "/status"}
-                )
-        print(f"method=GET, path=/status: {status_check_count} status check")
+    status_check_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
 
-    except Exception as e:
-        print(f"Error: {e}")
+
+def main():
+    """ main """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+
+    display_nginx_log_stats(nginx_collection)
 
 
 if __name__ == "__main__":
-    try:
-        client = MongoClient('mongodb://localhost:27017')
-        db = client.logs
-        nginx_collection = db.nginx
-
-        log_stats(nginx_collection)
-
-    except Exception as e:
-        print(f"Error connecting to MongoDB: {e}")
+    main()
